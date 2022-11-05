196f9dda84734614b8f82d42690d63cbcb2bb1f6
Inhoob <>
2022-11-05 17:10:47 +0900

    채널메뉴 UI 개발

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,10 +1,17 @@
-import { Box } from "@mui/material";
+import { Box, Drawer, Toolbar } from "@mui/material";
+import ChannelMenu from "../components/ChannelMenu";
 import Header from "../components/Header";
 const Main = () => {
   return (
     /** backgroundColor는 테마적용 */
     <Box sx={{ display: "flex", backgroundColor: "white" }}>
-      <Header></Header>
+      <Header />
+      <Drawer variant="permanent" sx={{ width: 300 }} className="no-scroll">
+        <Toolbar />
+        <Box sx={{ display: "flex", minHeight: "calc(100vh-64px" }}>
+          <ChannelMenu />
+        </Box>
+      </Drawer>
     </Box>
   );
 };
